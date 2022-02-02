d962fff593c6909343bd808751269cc608454a8a
Mark-Gallagher <G00343007@gmit.ie>
2022-02-02 10:01:05 +0000

    "New Test"

--- src/test/java/ie/gmit/CalculatorTest.java
+++ src/test/java/ie/gmit/CalculatorTest.java
@@ -1,10 +1,24 @@
 package ie.gmit;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
     @Test
-    void justAnExample() {
+    void TestExample() {
+
+
     }
+    @Test
+    public void addTest() {
+        Calculator calc = new Calculator();
+        int result = calc.add(4,8);
+        assertEquals(12,result,0);
+
+    }
+    @Test
+    public void subtractTest(){
+
+    }
+
 
 }
