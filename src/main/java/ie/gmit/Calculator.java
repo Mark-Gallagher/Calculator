aecb57670c1cac168b76f18d75d8188035a0a4ca
Mark_Gallagher <G00343007@gmit.ie>
2022-02-21 17:12:32 +0000

    "Subtracted two numbers and checked the values were within a certain range "

--- src/main/java/ie/gmit/Calculator.java
+++ src/main/java/ie/gmit/Calculator.java
@@ -9,22 +9,28 @@ public class Calculator {
 
     public int add(int firstnum, int secondnum) {
         //This method will add two numbers and check the values are within a certain range
-        if (firstnum > 0 && secondnum > 0 ){
+        if (firstnum > 0 && firstnum < 1000 && secondnum > 0 && secondnum < 1000){
             int result = firstnum + secondnum;
             System.out.println(result);
             return result;
         }
+
         else {
             throw new IllegalArgumentException("Not a valid entry!");
         }
 
     }
-
     public int subtract(int firstnum, int secondnum){
-        //this Method will subtract two numbers
-        int result = firstnum - secondnum;
-        System.out.println(result);
-        return result;
+        //This method will subtract two numbers and check the values are within a certain range
+        if (firstnum > 0 && firstnum < 1000 && secondnum > 0 && secondnum < 1000 ){
+            int result = firstnum - secondnum;
+            System.out.println(result);
+            return result;
+        }
+
+        else {
+            throw new IllegalArgumentException("Not a valid entry!");
+        }
     }
     public int divide(int firstnum, int secondnum){
         //this Method will subtract two numbers
