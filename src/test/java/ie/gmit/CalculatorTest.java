17c6bc5063f03bc1a421007ab213e393725f2ec7
Mark_Gallagher <G00343007@gmit.ie>
2022-03-05 21:10:30 +0000

    Added subtract and add tests

--- src/test/java/ie/gmit/CalculatorTest.java
+++ src/test/java/ie/gmit/CalculatorTest.java
@@ -1,9 +1,11 @@
 package ie.gmit;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 import org.junit.jupiter.api.BeforeEach;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
@@ -44,6 +46,44 @@ public class CalculatorTest {
         int result = calc.multiply(9,8);
         assertEquals(72,result,0);
     }
+
+
+    @DisplayName("Addition pass test")
+    @Test
+    void testAdd(){assertEquals(22,calc.add(12,10));}
+
+
+    @DisplayName("Addition fail test for Value less than 0")
+    @Test
+    void testAddFail(){
+        Exception exception = assertThrows(IllegalArgumentException.class,() -> calc.add(-12,10));
+        assertEquals("Not a valid entry!", exception.getMessage());
+    }
+    @DisplayName("Addition fail test for Value greater than 1000")
+    @Test
+    void testAddFailTwo(){
+        Exception exception = assertThrows(IllegalArgumentException.class,() -> calc.add(1012,10));
+        assertEquals("Not a valid entry!", exception.getMessage());
+    }
+
+    @DisplayName("Subtraction pass test")
+    @Test
+    void testSubtract(){assertEquals(-15,calc.subtract(50,65));}
+
+
+    @DisplayName("Subtraction fail test for Value less than 0")
+    @Test
+    void testSubtractFail(){
+        Exception exception = assertThrows(IllegalArgumentException.class,() -> calc.subtract(-10,65));
+        assertEquals("Not a valid entry!", exception.getMessage());
+    }
+    @DisplayName("Subtraction fail test for Value greater than 1000")
+    @Test
+    void testSubtractFailTwo(){
+        Exception exception = assertThrows(IllegalArgumentException.class,() -> calc.subtract(1065,65));
+        assertEquals("Not a valid entry!", exception.getMessage());
+    }
+
 
 }
 
