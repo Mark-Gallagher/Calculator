5c8d5882013ce45abd049d9e72f3c122e6346d93
Mark-Gallagher <92432812+Mark-Gallagher@users.noreply.github.com>
2022-03-05 19:22:00 +0000

    Update CalculatorTest.java

--- src/test/java/ie/gmit/CalculatorTest.java
+++ src/test/java/ie/gmit/CalculatorTest.java
@@ -15,6 +15,7 @@ public class CalculatorTest {
     public void setUp(){
        calc = new Calculator();
     }
+   
     @Test
     public void addTest() {
         //Calculator calc = new Calculator();
@@ -22,41 +23,24 @@ public class CalculatorTest {
         assertEquals(12,result,0);
 
     }
+   
     @Test
     public void subtractTest(){
         //Calculator calc = new Calculator();
-    void TestExample() {
-
-
-    }
-    @Test
-    public void addTest() {
-        Calculator calc = new Calculator();
-        int result = calc.add(4,8);
-        assertEquals(12,result,0);
-
-    }
-    @Test
-    public void subtractTest(){
-        Calculator calc = new Calculator();
         int result = calc.subtract(10,3);
         assertEquals(7,result,0);
     }
 
     @Test
     public void divideTest(){
-
         //Calculator calc = new Calculator();
-        Calculator calc = new Calculator();
         int result = calc.divide(48,12);
         assertEquals(4,result,0);
     }
 
     @Test
     public void multiplyTest(){
-
         //Calculator calc = new Calculator();
-        Calculator calc = new Calculator();
         int result = calc.multiply(9,8);
         assertEquals(72,result,0);
     }
